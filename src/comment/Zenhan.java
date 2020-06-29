89675639a09b90a80c160c8e82bcb8b542cdc349
hiroki369 <kissjoel676@gmail.com>
2020-06-29 12:04:24 +0900

    [Finish] chapter 6

---
+++ src/comment/Zenhan.java
@@ -0,0 +1,9 @@
+package comment;
+public class Zenhan {
+	public static void doWarusa() {
+		System.out.println("きなこでござる。くえませんがの。");
+	}
+	public static void doTogame() {
+		System.out.println("この老いぼれの目はごまかせませんぞ");
+	}
+}
